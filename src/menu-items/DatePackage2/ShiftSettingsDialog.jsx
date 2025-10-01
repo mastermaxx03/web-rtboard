8c4938fff9f6f1004c840a8b1921d4559c2f222b
mastermaxx03 <srivastavaanimesh22@gmail.com>
2025-10-01 12:37:34 +0530

    updated 2

--- src/menu-items/DatePackage2/ShiftSettingsDialog.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import UnifiedSettingsDialog from './UnifiedSettingsDialog';
-
-export default function ShiftSettingsDialog({ open, onClose }) {
-  return <UnifiedSettingsDialog open={open} onClose={onClose} initialPane="shifts" />;
-}
--- src/menu-items/DatePackage2/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DatePackage } from './DatePackage';
-export { ShiftSettingsProvider, useShifts } from './ShiftSettingsContext';
-export { ZoneSettingsProvider, useZones } from './ZoneSettingsContext';
