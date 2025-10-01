cf45c9a8f65a54293bdfbe0e8aef462fc052c165
mastermaxx03 <srivastavaanimesh22@gmail.com>
2025-10-01 12:32:47 +0530

    updates

--- src/views/realtime-layout/RT-Cost/utils/formatters.js
+++
@@ -1,3 +0,0 @@
-export const inr = (n) => (n ?? 0).toLocaleString('en-IN', { style: 'currency', currency: 'INR', maximumFractionDigits: 2 });
-
-export const nf2 = (n) => (n ?? 0).toLocaleString('en-IN', { maximumFractionDigits: 2, minimumFractionDigits: 2 });
